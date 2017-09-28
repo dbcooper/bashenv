3de561818eda56413d22e3eeb06cc1118d103483
Niall Durham <niall.durham@gmail.com>
2017-09-28 18:52:23 -0500

    First commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./install-functions
+
+# Link to system git bash completion script (if installed)
+if [ -r /usr/share/bash-completion/completions/git ]
+then
+    mklink /usr/share/bash-completion/completions/git ~/.git-completion.bash
+fi
+
+mklink $PWD/inputrc  ~/.inputrc
+mklink $PWD/mybashrc ~/.mybashrc
+
+# Load our bash magic on login
+if [ -r ~/.bashrc ]
+then
+    append ~/.bashrc bash-append
+else
+    echo "?No .bashrc file in $HOME, unsure how to proceed"
+    # TODO  Create simple .bashrc?
+fi
+
