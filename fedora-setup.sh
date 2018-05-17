635bb1a19d7ecc2ccfd4887a66c0f28ab50872d7
Niall Durham <niall.durham@tamucc.edu>
2018-05-17 11:58:53 -0500

    Try to auto-install Monaco font

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO  Test for sudo/root privileges?
+# This is intended to be run just once
 
-# Just trying to cover Fedora 27/28 (for now), assuming desktop/workstation
 fedora_rpms="
     ack
 
@@ -26,7 +25,20 @@ fedora_rpms="
 # Repository setup, then RPMs
 
 # From  https://rpmfusion.org/Configuration#Command_Line_Setup_using_rpm
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf install $fedora_rpms
+sudo dnf install $fedora_rpms
 
+# Install monaco font from https://github.com/inoyatov/monaco
+# XXX  This installs the font but I can't select it in gnome-terminal anyway :\
+font_fn=Ubuntu_Mono_derivative_Powerline.ttf
+curl -L --output $font_fn  https://github.com/inoyatov/monaco/raw/master/font/Ubuntu%20Mono%20derivative%20Powerline.ttf
+mkdir -p ~/.fonts
+mv $font_fn ~/.fonts
+
+symbols_fn=10-powerline-symbols.conf
+mkdir -p ~/.config/fontconfig/conf.d/
+curl -L --output $symbols_fn  https://raw.githubusercontent.com/inoyatov/monaco/master/config/10-powerline-symbols.conf
+mv $symbols_fn ~/.config/fontconfig/conf.d/
+
+fc-cache
