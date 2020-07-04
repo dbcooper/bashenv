dcc7809a1ee030280cf40eaf88c7bb262537c582
Niall Durham <niall.durham@tamucc.edu>
2020-07-04 15:22:47 -0500

    Windows vim setup/integration++

--- windows-install.ps1
+++ windows-install.ps1
@@ -11,10 +11,16 @@ $autoload_path  = "$my_home\vimfiles\autoload"
 $plug_file      = "$autoload_path\plug.vim"
 # Have to put configuration all vim-plug stuff in .vimrc:
 # https://github.com/junegunn/vim-plug/issues/540#issuecomment-254580340
+
+# Git bash windows mapping
 $vimrc_file     = "$my_home\.vimrc"
 $gvimrc_file    = "$my_home\.gvimrc"
 $gitconfig_file = "$my_home\.gitconfig"
 $bashrc_file    = "$my_home\.bashrc"
+
+# Windows Vim configuration file mappings
+$win_vimrc_file = "$my_home\_vimrc"
+$win_gvimrc_file = "$my_home\_gvimrc"
 
 # Install vimplug support  https://github.com/junegunn/vim-plug#windows-powershell
 If ( !(Test-Path $plug_file) ) {
@@ -29,22 +35,28 @@ If ( !(Test-Path $plug_file) ) {
     )
 }
 
-# TODO  Install vimplug for git bash's vim ~/.vim/autoload/plug.vim
-
 # TODO  Change tests to check latest modified time and copy over if repo version is newer
 If ( !(Test-Path $bashrc_file) ) {
     Copy-Item windows-bashrc -Destination $bashrc_file
 }
 
+# Git bash vim setup
 If ( !(Test-Path $vimrc_file) ) {
     Copy-Item unix-vimrc -Destination $vimrc_file
 }
-
 If ( !(Test-Path $gvimrc_file) ) {
     Copy-Item windows-gvimrc -Destination $gvimrc_file
 }
 
-Write-Output "You'll need to start gvim.exe and run :PlugInstall from git bash, probably."
+# Windows-specific gvim file setup
+If ( !(Test-Path $win_vimrc_file) ) {
+    Copy-Item unix-vimrc -Destination $win_vimrc_file
+}
+If ( !(Test-Path $win_gvimrc_file) ) {
+    Copy-Item windows-gvimrc -Destination $win_gvimrc_file
+}
+
+Write-Output "In the git bash shell, start vim and run :PlugInstall.  After that, plugins _should_ work properly in Windows gvim as well."
 
 If ( !(Test-Path $gitconfig_file) ) {
     Copy-Item gitconfig -Destination $gitconfig_file
