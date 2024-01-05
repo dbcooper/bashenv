4cc6f11216c76ed91b57bf5bb09f5a3dbb7a7324
Niall Durham <niall.durham@tamucc.edu>
2024-01-04 19:10:09 -0600

    Prompt and install Fedora-specific software

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-# This is intended to be run just once
+# NOTE: This is intended to be run just once
+# Additional Fedora-specific [Linux] configuration
+
+# Prompt user and don't quit until we get a [non-empty] repsonse
+# Returns lowercase version of whatever they typed in $response
+user_prompt() {
+    local prompt=$1                     # prompt text
+    local userinput
+
+    # Endlessly prompt until user enters something
+    while [[ -z "$userinput" ]]
+    do
+        read -p "$prompt" userinput
+    done
+    response=$(echo $userinput | tr A-Z a-z)
+}
+
 
 fedora_rpms="
     ack
@@ -31,9 +47,9 @@ fedora_rpms="
 # Repository setup, then RPMs/RPM groups
 
 # From  https://rpmfusion.org/Configuration#Command_Line_Setup_using_rpm
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf group install 'Development Tools'  'C Development Tools and Libraries'
+sudo dnf group install -y 'Development Tools'  'C Development Tools and Libraries'
 sudo dnf install $fedora_rpms
 
 # Install monaco font from https://github.com/inoyatov/monaco
@@ -57,12 +73,12 @@ fc-cache
 cpan App::cpanminus
 cpanm --sudo App::perlbrew
 
-
-# TODO  Prompt for confirmation before doing the following (default: No)
-
 # Optional [Full] desktop setup: more applications (RPMs) and conveniences
 # XXX  As of Fedora 38, qqc2-desktop-style is required by ksnip but not included in the existing RPM?
 more_rpms="
+    terminus-fonts
+    cascadia-code-pl-fonts
+
     gnome-tweaks
     fonts-tweak-tool
     tilix
@@ -81,22 +97,43 @@ more_rpms="
     conky
 "
 
-# If want VSCode:
-#   *  Add VSCode repository
-#
-# Copied directly from  https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
-##sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-##sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-#
-#   *  Add "code" RPM to the to-be-installed RPM list.  Will probably need to `dnf check-update` first
+compact=$(echo $more_rpms | sed 's/\s+/ /g;')
+echo "Do you want to install the rest of the GUI apps?  $compact"
+user_prompt 'Type "yes" to install, or anything else to skip [besides Enter]: '
 
-# TODO  Install additional RPMs
+if [[ "$response" == 'yes' ]]
+then
+    sudo dnf install -y $more_rpms
+fi
 
-# Google Chrome - automate install?
+echo "Do you want to install VSCode and Chrome?"
 
-# TODO  Ask/confirm, re: need to SSH into box
-# Ensure SSH daemon is setup
-#sudo systemctl start sshd.service
-#sudo systemctl enable sshd.service
+user_prompt 'Type "yes" to install, or anything else to skip [besides Enter]: '
+if [[ "$response" == 'yes' ]]
+then
+    # Copied directly from  https://code.visualstudio.com/docs/setup/linux#_rhel-fedora-and-centos-based-distributions
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+    # Add Google Chrome [repository]
+    # From https://docs.fedoraproject.org/en-US/quick-docs/installing-chromium-or-google-chrome-browsers/
+    sudo dnf config-manager --set-enabled google-chrome
+
+    # Add "code" RPM to the to-be-installed RPM list.  Will probably need to `dnf check-update` first
+    sudo dnf check-update
+
+    # Install VSCode and Chrome
+     sudo dnf install -y code google-chrome-stable
+fi
+
+# Ask/confirm, re: need to SSH into box
+echo "Do you want to setup SSH access to this machine?"
+user_prompt 'Type "yes" to setup SSH, or anything else to skip [besides Enter]: '
+if [[ "$response" == 'yes' ]]
+then
+    # Ensure SSH daemon is setup
+    sudo systemctl start sshd.service
+    sudo systemctl enable sshd.service
+fi
 
 # TODO  GUI keyboard shortcuts for terminal(s)
